3aba196da629cf738e29172faa542572218f2d70
lokendra <lokendrachaulagain803@gmail.com>
2022-03-31 19:39:00 +0545

    jwt

--- routes/auth.js
+++ routes/auth.js
@@ -18,8 +18,11 @@ router.post('/register', async (req, res) => {
       email: req.body.email,
       password: hashedPassword,
     })
+
+    //saving the user
     const user = await newUser.save()
-    res.status(200).json(user)
+    //if error occured during saving the user
+    return res.status(200).json(user)
 
     //if error occurs catch it
   } catch (error) {
@@ -27,19 +30,19 @@ router.post('/register', async (req, res) => {
   }
 })
 
-//LOGIN USER
+//LOGIN
 router.post('/login', async (req, res) => {
   try {
     //find the user by email
-    const user = await User.findOne({ email: req.body.email })
+    const user = await User.findOne({ email: req.body.email }) //req.body.email means the email given by the user
 
     //if user not found return error
     if (!user) {
       return res.status(400).json({ msg: 'User not found' })
     }
 
-    //If user found compare the user given password with hashed password
-    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    //If user found then compare the password given by user with hashed password in database
+    const validPassword = await bcrypt.compare(req.body.password, user.password) //req.body.password means the password given by the user and user.password means the hashed password in database
 
     //if password is not match return error
     if (!validPassword) {
@@ -47,18 +50,19 @@ router.post('/login', async (req, res) => {
     }
 
     //if password is match create a token//it will hide the info into the token------
-    const accessToken = jwt.sign(
-      { _id: user._id, isAdmin: user.isAdmin },
-      process.env.TOKEN_SECRET,
-      { expiresIn: '10d' }, //after this days the token will be expired so we should login again
-    )
+    // const accessToken = jwt.sign(
+    //   { _id: user._id, isAdmin: user.isAdmin },
+    //   process.env.TOKEN_SECRET,
+    //   { expiresIn: '10d' }, //after this days the token will be expired so we should login again
+    // )
     //------------------------------------------------------------------------
 
     //things that shouldnot be visible to the user
-    const { password, ...others } = user._doc
+    const { password, ...others } = user._doc //password private other show to the user
 
     //if user is found and password is match return user
-    res.status(200).json({...others, accessToken})
+    // res.status(200).json({...others, accessToken})
+    res.status(200).json({ others })
 
     //if error catch it and show error
   } catch (error) {
