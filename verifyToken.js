a8bf4f9b3e496c2b5c71ce0564d0c618bb3e8513
lokendra <lokendrachaulagain803@gmail.com>
2022-04-01 07:31:02 +0545

    jwt auth for update and delete

--- verifyToken.js
+++ verifyToken.js
@@ -1,22 +1,19 @@
 const jwt = require('jsonwebtoken')
-const  dotenv = require('dotenv').config()
+const JWT_SECRET = process.env.JWT_SECRET
+
 function verify(req, res, next) {
   const authHeader = req.headers.token
 
-  //if we gave a token lets split it and get the token
+  //we gave token with Bearer lets split it and get the token only
   if (authHeader) {
     const token = authHeader.split(' ')[1]
 
     //now we have a token lets verify it
-    jwt.verify(token,"mySecretKey", (err, user) => {
+    jwt.verify(token, JWT_SECRET, (err, user) => {
       if (err) {
         return res.status(403).json({ msg: 'Invalid token' })
       }
-
-      //if token is valid we will add the user to the request
       req.user = user
-
-      //next means go to the actual router
       next()
     })
   } else {
