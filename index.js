928e6b07b6effcd212f765cb419578f76416cf62
lokendra <lokendrachaulagain803@gmail.com>
2022-04-22 12:28:07 +0545

    movie and series list api completed

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv').config()
 const authRoute = require('./routes/auth')
 const userRoute = require('./routes/users')
 const movieRoute = require('./routes/movies')
+const listRoute = require('./routes/lists')
 
 
 //MongoDB connection
@@ -23,6 +24,7 @@ app.use(express.json()) //accept json data
 app.use('/api/auth/', authRoute)
 app.use('/api/users/', userRoute)
 app.use('/api/movies/', movieRoute)
+app.use('/api/lists/', listRoute)
 
 
 //app listening on port
